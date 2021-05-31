53dd82deb7eb228cdd95f19add417b9c87f617df
Thijs Daniels <thijs.daniels@hotmail.com>
2021-05-31 11:57:24 +0200

    Added multipage support

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import dash
+import dash_bootstrap_components as dbc
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = [dbc.themes.BOOTSTRAP]
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__,
+                # these meta_tags ensure content is scaled correctly on different devices
+                # see: https://www.w3schools.com/css/css_rwd_viewport.asp for more
+                meta_tags=[
+                    {"name": "viewport", "content": "width=device-width, initial-scale=1"}
+                ],
+                external_stylesheets=external_stylesheets)
 
 server = app.server
 app.config.suppress_callback_exceptions = True
---
+++ apps/homepage.py
@@ -0,0 +1,93 @@
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+from dash.dependencies import Input, Output, State
+import dash_table
+import dash
+
+import plotly.graph_objects as go
+import plotly.express as px
+
+import pandas as pd
+import pathlib
+import yaml
+from functions import *
+from styling import *
+
+from app import app
+
+# App Layout
+#app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+navbar = dbc.NavbarSimple(
+    brand="Palade - Data Quality Dashboard",
+    brand_href="#",
+    color="#567C4D",
+    dark=True
+)
+
+sidebar = dbc.Container([
+    dbc.Row([
+        html.Div(
+            [
+                html.H2("Home", className="display-4"),
+                html.Hr(),
+                html.P(
+                    "A simple sidebar layout with navigation links", className="lead"
+                ),
+                dbc.Nav(
+                    [
+                        dbc.NavLink("Home", href="/", active="exact"),
+                        dbc.NavLink("Overview", href="/overview", active="exact"),
+                        dbc.NavLink("Completeness", href="/completeness", active="exact"),
+                        dbc.NavLink("Uniqueness", href="/uniqueness", active="exact"),
+                        dbc.NavLink("Validity", href="/validity", active="exact"),
+                        dbc.NavLink("Accuracy", href="/accuracy", active="exact"),
+                        dbc.NavLink("Data", href="/data", active="exact")
+                    ],
+                    vertical=True,
+                    pills=True,
+                ),
+            ],
+        ),
+    ], className='h-100')
+])
+
+
+content = dbc.Container([], style={
+    "height": "100vh",
+    "margin": "0vh",
+    "border": "0vh",
+    "padding": "0vh",
+    "max-width": '100%',
+})
+
+layout = dbc.Container([
+    navbar,
+    dbc.Row([
+        dbc.Col(html.Div(sidebar),
+                width=2, style={
+                "margin": "0",
+                "border": "0",
+                "padding-left": '3vh',
+                "height": "100%",
+                "background-color": "#f8f9fa"
+                }),
+        dbc.Col(html.Div(content),
+                width=10),
+    ], style={
+        "margin": "0",
+        "border": "0",
+        "padding": "0",
+        "height": "100vh"}),
+], style={
+    'padding-right': '0',
+    'padding-left': '0',
+    'margin-right': '0',
+    'margin-left': '0',
+    'max-width': '100%'
+})
+
+
+#if __name__ == '__main__':
+#   app.run_server(debug=True)
